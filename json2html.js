cb5f25024a98196f1c59932095296672be271ccc
Ayush Sharma <ayushsharma290802@gmail.com>
2024-11-06 14:53:51 +0530

    Update json2html.js

--- json2html.js
+++ json2html.js
@@ -1,18 +1,29 @@
+// json2html.js
+
 export default function json2html(data) {
+  // Get unique keys from the data array to create table headers
+  const headers = [...new Set(data.flatMap(Object.keys))];
+
+  // Build the HTML table
+  let html = `<table data-user="ayushsharma290802@gmail.com">`;
   
-    const headers = [...new Set(data.flatMap(Object.keys))];
+  // Create table header
+  html += "<thead><tr>";
+  headers.forEach(header => {
+    html += `<th>${header}</th>`;
+  });
+  html += "</tr></thead>";
   
-    let html = `<table data-user="ayushsharma290802@gmail.com">`;
-    html += `<thead><tr>${headers.map(header => `<th>${header}</th>`).join('')}</tr></thead>`;
-    html += `<tbody>`;
-  
-   
-    data.forEach(row => {
-      html += `<tr>${headers.map(header => `<td>${row[header] || ''}</td>`).join('')}</tr>`;
+  // Create table body
+  html += "<tbody>";
+  data.forEach(row => {
+    html += "<tr>";
+    headers.forEach(header => {
+      html += `<td>${row[header] || ''}</td>`; // If a property is missing, leave cell blank
     });
+    html += "</tr>";
+  });
+  html += "</tbody></table>";
   
-    html += `</tbody></table>`;
-  
-    return html;
-  }
-  
+  return html;
+}
